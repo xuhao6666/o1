04f105b2e9c9560f36f0ee58a7aa135ddf7e5047
xuhao6666 <45914112+xuhao6666@users.noreply.github.com>
2018-12-16 20:40:18 +0800

    Update FristClass.java

--- git-test-project/src/com/itheima/FristClass.java
+++ git-test-project/src/com/itheima/FristClass.java
@@ -4,5 +4,7 @@ public class FristClass {
     public static void main(String[] args) {
         System.out.println("第一个类");
         System.out.println("第二个类")
+        System.out.println("第三个类")
+        
     }
 }
